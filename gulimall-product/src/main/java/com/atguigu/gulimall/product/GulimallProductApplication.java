b17f91b044a7b39c126c1595f42d4f9be451acae
MorleySu <951666991@qq.com>
2022-12-03 14:03:44 +0800

    P23学完

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,18 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/*
+* 整合mybatis-plus
+* 1）导入依赖
+* 2) 配置
+*
+* */
+@MapperScan("com.atguigu.gulimall.product.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallProductApplication {
 
